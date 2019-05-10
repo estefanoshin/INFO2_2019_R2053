c67d06d179603e3b80d3255a459a9f6f12bb7335
estefanoshin <estefanoshin@yahoo.com.ar>
2019-05-10 20:17:26 -0300

    proyecto iniciado

---
+++ practicas/practica_1/Debug/src/INFO2_LIB.d
@@ -0,0 +1,19 @@
+src/INFO2_LIB.o src/INFO2_LIB.d: ../src/INFO2_LIB.c \
+ /home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/xyz_timer.h \
+ /home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/registros.h \
+ /home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/lpc_types.h \
+ /home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/xyz_base.h \
+ /home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/xyz_io.h \
+ /home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/xyz_base.h
+
+/home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/xyz_timer.h:
+
+/home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/registros.h:
+
+/home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/lpc_types.h:
+
+/home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/xyz_base.h:
+
+/home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/xyz_io.h:
+
+/home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc/xyz_base.h:
---
+++ practicas/practica_1/Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/INFO2_LIB.c \
+../src/cr_startup_lpc175x_6x.c \
+../src/crp.c 
+
+OBJS += \
+./src/INFO2_LIB.o \
+./src/cr_startup_lpc175x_6x.o \
+./src/crp.o 
+
+C_DEPS += \
+./src/INFO2_LIB.d \
+./src/cr_startup_lpc175x_6x.d \
+./src/crp.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -I"/home/oem/Documents/info2/INFO2_2019_R2053/practicas/practica_1/inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
