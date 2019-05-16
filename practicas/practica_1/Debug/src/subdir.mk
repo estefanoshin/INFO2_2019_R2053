8de48950b13de73cb10db51f44fbe5f0ea71ff23
estefano shin <estefano.shin@galenoargentina.com.ar>
2019-05-16 15:31:24 -0300

    modificaciones

---
+++ practicas/practica_1/Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/cr_startup_lpc175x_6x.c \
+../src/crp.c \
+../src/practica_1.c 
+
+OBJS += \
+./src/cr_startup_lpc175x_6x.o \
+./src/crp.o \
+./src/practica_1.o 
+
+C_DEPS += \
+./src/cr_startup_lpc175x_6x.d \
+./src/crp.d \
+./src/practica_1.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M3 -D__LPC17XX__ -D__REDLIB__ -I"C:\Users\eshin\Desktop\INFO2_2019_R2053\practicas\practica_1\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
--- practicas/practica_1/src/practica_1.c
+++ practicas/practica_1/src/practica_1.c
@@ -22,61 +22,75 @@
 
 #include <xyz_base.h>
 
-// TODO: insert other include files here
+#define TRUE 1
+#define FALSE 0
+#define ON 1
+#define OFF 0
 
-// TODO: insert other definitions and declarations here
+#define TIMER_SECONDS 10
+
+#define IS_FIN_CARRERA_ABIERTO Teclado(4)
+#define IS_FIN_CARRERA_CERRADO Teclado(3)
+
+#define ABRIR(state) Relays(1, state)
+#define CERRAR(state) Relays(2, state)
+
+#define SENSOR_PUERTA_EXTERNA Dig_Inputs(1)
+#define SENSOR_PUERTA_INTERNA Dig_Inputs(2)
+
+#define INIT_TIMER(time) set_timer(0,time)
+#define IS_TIMEOUT is_timer_end(0)
+
+enum STATES {ABIERTO, CERRADO, ABRIENDO, CERRANDO};
 
 int main(void)
 {
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
+	INIT_TIMER(TIMER_SECONDS);
+	CERRAR(ON);
+	static STATES estado = CERRADO;
 
-    int8_t final0 = 0, final1 =0, final2 = 0,final3 = 0;
-    int8_t in1 = 0, in2 = 0;
-
-    // ver de inicializar io
-
-    Inicializar_io();
-
-    InitTimer();
-
-    set_timer(0,10);
-
-    // Enter an infinite loop, just incrementing a counter
     while(1)
     {
-    	if((final0=is_timer_end(0)))
-            final0 = 4;
+    	switch (estados)
+    		    	{
+    					case ABIERTO:
+    						ABRIR(OFF);
+    						if((SENSOR_PUERTA_EXTERNA == FALSE && SENSOR_PUERTA_INTERNA == FALSE) || IS_TIMEOUT == TRUE)
+    							estado = CERRANDO;
+    						break;
 
-    	final0 = Teclado(1);          // P0.18
-    	final1 = Teclado(2);          // P0.11
-    	final2 = Teclado(3);          // P2.13
-    	final3 = Teclado(4);          // P1.16
+    					case CERRADO:
+    						CERRAR(OFF);
+    						if(SENSOR_PUERTA_EXTERNA == TRUE || SENSOR_PUERTA_INTERNA == TRUE)
+    							estado = ABRIENDO;
+    						break;
 
-    	in1 = Dig_Inputs(1);          // P4.29
-    	in2 = Dig_Inputs(2);          // P2.11
+    					case ABRIENDO:
+    						CERRAR(OFF);
+    						ABRIR(ON);
+    						if((SENSOR_PUERTA_EXTERNA == FALSE && SENSOR_PUERTA_INTERNA == FALSE) || IS_TIMEOUT == TRUE)
+    							estado = CERRANDO;
+    						break;
 
-    	Relays (0 , ON );             // P2.0
-    	Relays (0 , OFF);
-    	Relays (1 , ON);              // P0.23
-    	Relays (1 , OFF);
-    	Relays (2 , ON );             // P0.21
-    	Relays (2 , OFF );
-/*
-    	Led (1, OFF);                 // P2.1
-    	Led (1, ON);
-    	Led (2, OFF);                 // P2.2
-    	Led (2, ON);
-    	Led (3, OFF);                 // P2.3
-*/
-        if(is_timer_end(0))
-        {
-    	  Led (0, OFF);
-    	  Led (1, OFF);
-    	  Led (2, OFF);
-        }
+    						if(IS_FIN_CARRERA_ABIERTO == TRUE)
+    							estado = ABIERTO;
+    						break;
 
-    	i++ ;
+    					case CERRANDO:
+    						ABRIR(OFF);
+    						CERRAR(ON);
+    						if((SENSOR_PUERTA_EXTERNA == TRUE || SENSOR_PUERTA_INTERNA == TRUE))
+    							estado = ABRIENDO;
+    						break;
+
+    						if(IS_FIN_CARRERA_CERRADO == TRUE)
+    							estado = CERRADO;
+    						break;
+
+    					default:
+    						//TODO ...
+    						break;
+    				}
     }
     return 0 ;
 }
